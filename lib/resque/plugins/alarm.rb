c4af754fde4a16847a19016e962f22287fc2654f
An Vo Nguyen Thien <an.vo@codebox.vn>
2015-11-24 15:57:11 +0700

    Add log to debug

--- lib/resque/plugins/alarm.rb
+++ lib/resque/plugins/alarm.rb
@@ -2,8 +2,28 @@ module Resque
   module Plugins
     module Alarm
       def after_enqueue_check_alarm(*args)
-        # magic come here
+        # queue = ""
+        # size = Resque.size(queue)
+        # if size > threshold
+        #   if Time.now - last_alarm > sleep_time
+        #     Mailer.alarm(queue, size)
+        #   end
+        # end
+        Rails.logger.info "Executing Alarm.after_enqueue_check_alarm"
+        puts "PUT: Executing Alarm.after_enqueue_check_alarm"
       end
+
+      private
+      def sleep_time
+        0
+      end
+      def threshold
+        0
+      end
+      def last_alarm
+        Time.now
+      end
+
     end
   end
 end
