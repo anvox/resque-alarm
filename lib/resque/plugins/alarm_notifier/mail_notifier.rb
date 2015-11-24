1a01ab3001ae3431ea8a3c44e99c37c93638e300
An Vo Nguyen Thien <an.vo@codebox.vn>
2015-11-25 00:47:07 +0700

    remove debug. fix wrong name.

--- lib/resque/plugins/alarm_notifier/mail_notifier.rb
+++ lib/resque/plugins/alarm_notifier/mail_notifier.rb
@@ -13,17 +13,14 @@ module Resque
           @params = params
           text = ERB.new(File.read(File.dirname(__FILE__) + "/#{@template}.html.erb")).result(binding)
           subject = "[#{@tags.join('][')}] #{@subject}"
-          puts text
-          puts "================"
-          puts subject
           MailNotifierMailer.mail_notifier_mailer(@from, @to, subject, text).deliver!
         end
       end
 
       class MailNotifierMailer < ::ActionMailer::Base
         def mail_notifier_mailer(from, to, subject, body)
-          mail :from => @from, :to => @to, :subject => subject do |format|
-            format.html { render :text => text }
+          mail :from => from, :to => to, :subject => subject do |format|
+            format.html { render :text => body }
           end
         end
       end
